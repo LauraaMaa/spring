8e983d51cff5a0493e6141eca60adde593b4e191
laura <laura.ma@gmail.com>
2023-05-02 09:28:06 +0300

    first commit

---
+++ src/test/java/com/spring/skaiciuotuvas/SkaiciuotuvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.skaiciuotuvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaiciuotuvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
